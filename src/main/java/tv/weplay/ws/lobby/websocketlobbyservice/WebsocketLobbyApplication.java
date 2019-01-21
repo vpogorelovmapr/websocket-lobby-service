b6e930d87af4abb40974030eebb3ccf472422d93
Gusan Katerina <dgivasss@gmail.com>
2019-01-21 17:32:31 +0200

    Initial commit

---
+++ src/main/java/tv/weplay/ws/lobby/websocketlobbyservice/WebsocketLobbyApplication.java
@@ -0,0 +1,14 @@
+package tv.weplay.ws.lobby.websocketlobbyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketLobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketLobbyApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/tv/weplay/ws/lobby/websocketlobbyservice/WebsocketLobbyServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package tv.weplay.ws.lobby.websocketlobbyservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsocketLobbyServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
